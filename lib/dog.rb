9359f1191cdf5f65f1b9901bb881046ec55e9937
David McGrann <dpmcgrann@gmail.com>
2018-07-13 15:07:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,7 @@ class Dog
     SQL
 
     result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0], result[1], result[2])
+    Dog.new(id: result[0], name: result[1], breed: result[2])
 
     
   end
