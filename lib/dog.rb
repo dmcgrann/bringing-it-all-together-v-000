1aa1e0ae04bf815bdfa41cc53128b70c81cdb604
David McGrann <dpmcgrann@gmail.com>
2018-07-13 15:08:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,9 @@ class Dog
 
     result = DB[:conn].execute(sql, id)[0]
     Dog.new(id: result[0], name: result[1], breed: result[2])
-
-    
   end
+  
+  def self.find_or_create_by
+    
   
 end
